a85c3ded08207ebbf1ca149eaaebb859e227a60b
C-D-Lewis <Bonsitm@gmail.com>
2023-03-04 13:29:32 +0000

    Update

--- scripts/shortcuts.py
+++ scripts/shortcuts.py
@@ -15,18 +15,7 @@ keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
 consumer_control = ConsumerControl(usb_hid.devices)
 
-# Shortcuts:
-# 0: vol up, 1: prev, 2: play/pause, 3: next
-# 4: vol down,
-# 8: -
-# 12: mute,
-
-# ConsumerControlCode.SCAN_PREVIOUS_TRACK
-# ConsumerControlCode.PLAY_PAUSE
-# ConsumerControlCode.SCAN_NEXT_TRACK
-
-# A map of keycodes that will be mapped sequentially to each of the keys, 0-15
-keymap = {
+KEY_MAP = {
   0: {
     'control_code': ConsumerControlCode.VOLUME_INCREMENT,
     'color': (64, 64, 0)
@@ -56,11 +45,7 @@ keymap = {
     'color': (64, 0, 0)
   },
   14: {
-    'sequence': [
-      (Keycode.GUI, Keycode.X),
-      Keycode.U,
-      Keycode.S
-    ],
+    'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.S],
     'color': (0, 0, 32)
   },
   15: {
@@ -73,55 +58,59 @@ keymap = {
 # Flash a key to confirm an action.
 #
 def flash_confirm(key):
-  key.set_led(255, 255, 255)
+  key.set_led(128, 128, 128)
   sleep(0.05)
   key.set_led(0, 0, 0)
   sleep(0.05)
-  key.set_led(255, 255, 255)
+  key.set_led(128, 128, 128)
   sleep(0.05)
   key.set_led(0, 0, 0)
-  sleep(0.05)
-  key.set_led(255, 255, 255)
-  sleep(0.05)
-  key.set_led(0, 0, 0)
-  sleep(0.05)
+
+#
+# Handle a key press event
+#
+def handle_key_press(key):
+  config = KEY_MAP[key.number]
+
+  # Control code
+  if 'control_code' in config:
+    consumer_control.send(config['control_code'])
+  
+  # Text string
+  if 'text' in config:
+    layout.write(config['text'])
+  
+  # Key combinationn
+  if 'combo' in config:
+    keyboard.press(*config['combo'])
+    keyboard.release_all()
+
+  # Sequence of key combinations
+  if 'sequence' in config:
+    for item in config['sequence']:
+      if isinstance(item, tuple):
+        keyboard.press(*item)
+      else:
+        keyboard.press(item)
+      sleep(0.2)
+      keyboard.release_all()
 
 # Attach handler functions to all of the keys
 for key in keys:
-  if key.number not in keymap:
+  if key.number not in KEY_MAP:
     continue
-  config = keymap[key.number]
-
-  # Set initial color
+  config = KEY_MAP[key.number]
   key.set_led(*config['color'])
 
-  # When pressed
   @keybow.on_press(key)
   def press_handler(key):
-    config = keymap[key.number]
-
-    if 'control_code' in config:
-      consumer_control.send(config['control_code'])
-    if 'text' in config:
-      layout.write(config['text'])
-    if 'combo' in config:
-      keyboard.press(*config['combo'])
-      keyboard.release_all()
-    if 'sequence' in config:
-      for item in config['sequence']:
-        if isinstance(item, tuple):
-          keyboard.press(*item)
-        else:
-          keyboard.press(item)
-        sleep(0.2)
-        keyboard.release_all()
-
+    handle_key_press(key)
     flash_confirm(key)
 
-  # When released
   @keybow.on_release(key)
   def release_handler(key):
-    key.set_led(*keymap[key.number]['color'])
+    key.set_led(*KEY_MAP[key.number]['color'])
 
+# Loop input detection forever
 while True:
   keybow.update()
