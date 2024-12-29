d6d7b71cef8c151d7010cf33ce4c4f9fc74f9613
Chris Lewis <Bonsitm@gmail.com>
2024-12-29 09:50:32 +0000

    update

--- keypad-base/macros/screensavers.py
+++ keypad-base/macros/screensavers.py
@@ -164,7 +164,7 @@ def update_screensaver(keys):
   hours_24h = (now[3] + config.TZ_OFFSET_H) % 24
 
   # No dazzling screensavers between 11 PM ana 9 AM
-  if hours_24h >= 23 or (hours_24h >= 0 and hours_24h <= 9):
+  if hours_24h >= 23 or (hours_24h >= 0 and hours_24h < 9):
     return
   elif config.SCREENSAVER == constants.SCREENSAVER_NONE:
     return
