56ff0f6d60e15f5b097bc006cb67e43c8c273990
Chris Lewis <Bonsitm@gmail.com>
2025-01-02 17:23:00 +0000

    Use named layers

--- keypad-base/macros/modules/macros.py
+++ keypad-base/macros/modules/macros.py
@@ -21,90 +21,94 @@ from modules import constants, screensavers
 #   'search'       - Search in Start menu and then press enter. Useful for installed apps.
 #
 def get_macro_map(keys):
-  return {
-    # Media layer
-    0: {
-      1: {
-        'control_code': ConsumerControlCode.SCAN_PREVIOUS_TRACK,
-        'color': constants.COLOR_BLUE
-      },
-      2: {
-        'control_code': ConsumerControlCode.PLAY_PAUSE,
-        'color': constants.COLOR_GREEN
-      },
-      3: {
-        'control_code': ConsumerControlCode.SCAN_NEXT_TRACK,
-        'color': constants.COLOR_BLUE
-      },
-      5: {
-        'control_code': ConsumerControlCode.VOLUME_DECREMENT,
-        'color': constants.COLOR_YELLOW
-      },
-      6: {
-        'control_code': ConsumerControlCode.MUTE,
-        'color': constants.COLOR_RED
-      },
-      7: {
-        'control_code': ConsumerControlCode.VOLUME_INCREMENT,
-        'color': constants.COLOR_YELLOW
-      },
-      # Discord mute (must add this custom keybind to work in background)
-      13: {
-        'combo': (Keycode.CONTROL, Keycode.SHIFT, Keycode.M),
-        'color': constants.COLOR_RED
-      }
-    },
-    # Applications layer
+  media_layer = {
     1: {
-      1: {
-        'search': 'spotify',
-        'color': constants.COLOR_GREEN
-      },
-      2: {
-        'search': 'steam',
-        'color': (0, 0, 16)
-      },
-      3: {
-        'search': 'discord',
-        'color': (0, 32, 64)
-      },
-      5: {
-        'search': 'firefox',
-        'color': (25, 0, 0)
-      }
+      'control_code': ConsumerControlCode.SCAN_PREVIOUS_TRACK,
+      'color': constants.COLOR_BLUE
     },
-    # Windows layer
     2: {
-      1: {
-        'combo': (Keycode.CONTROL, Keycode.SHIFT, Keycode.ESCAPE),
-        'color': (0, 32, 0)
-      },
-      2: {
-        'combo': (Keycode.GUI, Keycode.E),
-        'color': constants.COLOR_YELLOW
-      },
-      13: {
-        'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.S],
-        'color': (0, 0, 32)
-      },
-      14: {
-        'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.R],
-        'color': (0, 32, 0)
-      },
-      15: {
-        'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.U],
-        'color': (32, 0, 0)
-      }
+      'control_code': ConsumerControlCode.PLAY_PAUSE,
+      'color': constants.COLOR_GREEN
     },
-    # Other (meta, web? numpad?) layer
     3: {
-      14: {
-        'custom': lambda: screensavers.toggle_screensaver_disabled(keys),
-        'color': (16, 16, 16)
-      },
-      15: {
-        'custom': lambda: screensavers.start_screensaver(keys),
-        'color': (0, 0, 16)
-      }
+      'control_code': ConsumerControlCode.SCAN_NEXT_TRACK,
+      'color': constants.COLOR_BLUE
+    },
+    5: {
+      'control_code': ConsumerControlCode.VOLUME_DECREMENT,
+      'color': constants.COLOR_YELLOW
+    },
+    6: {
+      'control_code': ConsumerControlCode.MUTE,
+      'color': constants.COLOR_RED
+    },
+    7: {
+      'control_code': ConsumerControlCode.VOLUME_INCREMENT,
+      'color': constants.COLOR_YELLOW
+    },
+    # Discord mute (must add this custom keybind to work in background)
+    13: {
+      'combo': (Keycode.CONTROL, Keycode.SHIFT, Keycode.M),
+      'color': constants.COLOR_RED
     }
+  }
+
+  applications_layer = {
+    1: {
+      'search': 'spotify',
+      'color': constants.COLOR_GREEN
+    },
+    2: {
+      'search': 'steam',
+      'color': (0, 0, 16)
+    },
+    3: {
+      'search': 'discord',
+      'color': (0, 32, 64)
+    },
+    5: {
+      'search': 'firefox',
+      'color': (25, 0, 0)
+    }
+  }
+
+  windows_layer = {
+    1: {
+      'combo': (Keycode.CONTROL, Keycode.SHIFT, Keycode.ESCAPE),
+      'color': (0, 32, 0)
+    },
+    2: {
+      'combo': (Keycode.GUI, Keycode.E),
+      'color': constants.COLOR_YELLOW
+    },
+    13: {
+      'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.S],
+      'color': (0, 0, 32)
+    },
+    14: {
+      'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.R],
+      'color': (0, 32, 0)
+    },
+    15: {
+      'sequence': [(Keycode.GUI, Keycode.X), Keycode.U, Keycode.U],
+      'color': (32, 0, 0)
+    }
+  }
+
+  misc_layer = {
+    14: {
+      'custom': lambda: screensavers.toggle_screensaver_disabled(keys),
+      'color': (16, 16, 16)
+    },
+    15: {
+      'custom': lambda: screensavers.start_screensaver(keys),
+      'color': (0, 0, 16)
+    }
+  }
+
+  return {
+    0: media_layer,
+    1: applications_layer,
+    2: windows_layer,
+    3: misc_layer
   }
