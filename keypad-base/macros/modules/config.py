0631996f03910532c0173de964b20fd775b0743e
Chris Lewis <Bonsitm@gmail.com>
2025-01-04 11:08:37 +0000

    Config defaults

--- keypad-base/macros/modules/config.py
+++ keypad-base/macros/modules/config.py
@@ -1,15 +1,18 @@
 import os
 
+from modules import constants
+
 # Wi-Fi SSID
 WIFI_SSID = os.getenv('WIFI_SSID')
+
 # Wi-Fi password
 WIFI_PASSWORD = os.getenv('WIFI_PASSWORD')
 
 # The current screensaver - select from constants.py
-SCREENSAVER = os.getenv('SCREENSAVER')
+SCREENSAVER = os.getenv('SCREENSAVER', constants.SCREENSAVER_STARRY_NIGHT)
 
 # If WiFi is not configured, don't do anything NTP or internet related
 IS_WIFI_ENABLED = WIFI_SSID is not None and WIFI_PASSWORD is not None
 
 # Timezone offset in hours, such as BST
-TZ_OFFSET_H = int(os.getenv('TZ_OFFSET_H'))
+TZ_OFFSET_H = int(os.getenv('TZ_OFFSET_H', '0'))
